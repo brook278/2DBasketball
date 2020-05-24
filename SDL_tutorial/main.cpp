aaf2038dc227324b1e8c519032b6fe30df302e96
Jack <brook278@msu.edu>
2020-05-23 20:34:55 -0400

    Initial Commit

---
+++ SDL_tutorial/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SDL_tutorial
+//
+//  Created by Jack Brooks on 5/23/20.
+//  Copyright © 2020 SDL_tutorial. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
